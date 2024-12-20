2135eae3514a429fe7551d3ee4f92e18a8c847cb
zing <156516215+u-zing@users.noreply.github.com>
2024-12-20 23:28:20 +0900

    delete

--- js/background.js
+++ js/background.js
@@ -1,11 +1,31 @@
 const videos = ["002.mp4", "003.mp4", "004.mp4", "005.mp4", "006.mp4"];
-
 const choiceBackground = document.querySelector(".choice-bg");
 const audio = document.getElementById("space-audio");
 const toggleAudioBtn = document.querySelector(".toggle-audio");
+let currentBackground = null;
 
 function setRandomBackground() {
-  const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+  let availableVideos = videos.slice();
+
+  // 현재 배경 비디오가 있다면 그 비디오를 제외
+  if (currentBackground) {
+    availableVideos = availableVideos.filter(
+      (video) => video !== currentBackground
+    );
+  }
+
+  // 새로 랜덤 비디오 선택
+  const randomVideo =
+    availableVideos[Math.floor(Math.random() * availableVideos.length)];
+
+  // 현재 배경 비디오를 업데이트
+  currentBackground = randomVideo;
+
+  // 기존 비디오가 있으면 제거
+  const existingVideo = document.querySelector(".background-video");
+  if (existingVideo) {
+    existingVideo.remove();
+  }
 
   // 새로운 비디오 요소를 생성
   const backgroundVideo = document.createElement("video");
